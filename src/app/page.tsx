2c905345db8fc366e998a1d5919f68fa1a50f14d
dulranga <dulranga.webdesign@gmail.com>
2026-02-28 15:06:43 +0530

    added blog articles section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Blog from "~/components/Blog";
 import HeroSection from "~/components/HeroSection";
 import Projects from "~/components/Projects";
 
 export default function Page() {
-  return (
-    <main className="min-h-screen">
-      <HeroSection />
-      <Projects />
-    </main>
-  );
+    return (
+        <main className="min-h-screen">
+            <HeroSection />
+            <Projects />
+            <Blog />
+        </main>
+    );
 }
